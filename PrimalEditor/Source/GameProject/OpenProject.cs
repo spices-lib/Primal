ba427634ab05250106edec391cf4161ef251cee3
Spices <spiecs@outlook.com>
2026-08-09 23:15:49 +0800

    feat: add OpenProject

--- PrimalEditor/Source/GameProject/OpenProject.cs
+++ PrimalEditor/Source/GameProject/OpenProject.cs
@@ -1,6 +1,61 @@
-﻿namespace PrimalEditor.Source.GameProject;
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.Serialization;
 
-public class OpenProject
+namespace PrimalEditor.Source.GameProject;
+
+[DataContract]
+public class ProjectData
 {
-    
+    [DataMember]
+    public string ProjectName { get; set; }
+    [DataMember]
+    public string ProjectPath { get; set; }
+    [DataMember]
+    public DateTime Data { get; set; }
+}
+
+[DataContract]
+public class ProjectDataList
+{
+    [DataMember]
+    public List<ProjectData> Projects { get; set; }
+}
+
+class OpenProject
+{
+    private static readonly string _applicationDataPath =
+        $@"{Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData)}\PrimalEditor\";
+
+    private static readonly string _projectDataPath;
+    private static readonly ObservableCollection<ProjectData> _projects = new();
+    public static ReadOnlyObservableCollection<ProjectData> Projects { get; }
+
+    private static void ReadProjectData()
+    {
+
+    }
+
+    public static Project Open(ProjectData projectData)
+    {
+        ReadProjectData();
+    }
+
+    static OpenProject()
+    {
+        try
+        {
+            if (!Directory.Exists(_applicationDataPath)) Directory.CreateDirectory(_applicationDataPath);
+            _projectDataPath = $@"{_applicationDataPath}ProjectData.xml";
+            Projects = new(_projects);
+            ReadProjectData();
+        }
+        catch (Exception e)
+        {
+            Debug.WriteLine(e.Message);
+        }
+    }
 }
